97996e93808f91debd4b395b6f2a6c2cfedaa5df
Isaac Colls <colls_isaac@hotmail.com>
2020-02-13 18:28:11 -0300

    create token when register new user

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from .models import Movie, Rating
 
@@ -11,6 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
 
 
